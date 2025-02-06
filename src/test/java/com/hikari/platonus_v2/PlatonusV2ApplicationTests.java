e18e1c7fa54c4a5f695ef350499b3b604e3f8928
Nurbol <nurbol.bizhigit@gmail.com>
2025-02-06 18:05:11 +0500

    redo v2

---
+++ src/test/java/com/hikari/platonus_v2/PlatonusV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hikari.platonus_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatonusV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
